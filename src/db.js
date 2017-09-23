b379b405a76970309d8c54011a38719a35f56eb0
iamdudeman <timothy.solum@gmail.com>
2017-09-23 11:17:53 -0500

    Added test file for app

--- src/db.js
+++ src/db.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 const DATA_FILE = 'db.json';
 
 class db {
---
+++ test/app-test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('app', () => {
+    describe('stuff', () => {
+        it('1=1', () => {
+            assert.equal(1, 1);
+        });
+    });
+});
