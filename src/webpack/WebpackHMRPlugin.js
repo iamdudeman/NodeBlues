e636341bbf0bf6b8535dc05a4b3bc838b2757e6b
iamdudeman <timothy.solum@gmail.com>
2017-11-22 23:11:57 -0600

    Switched to using on-build-webpack

--- src/webpack/WebpackHMRPlugin.js
+++ src/webpack/WebpackHMRPlugin.js
@@ -1,7 +1,17 @@
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackOnBuildPlugin = require('on-build-webpack');
 
 module.exports = function createWebpackHMRPlugin(host, port) {
-    return new WebpackShellPlugin({
-        onBuildExit: [`node ./triggerHMR.js ${host} ${port}`]
+    return new WebpackOnBuildPlugin(() => {        
+        const WebSocket = require('ws');
+        const ws = new WebSocket(`ws://${host}:${port}`);
+        
+        ws.on('open', () => {
+            ws.on('message', data => {
+                if (JSON.parse(data).HMR) {
+                    ws.terminate();
+                }
+            });
+            ws.send(JSON.stringify({HMR: true}));
+        });
     });
 };
--- test/webpack/WebpackHMRPluginTest.js
+++ test/webpack/WebpackHMRPluginTest.js
@@ -2,10 +2,19 @@ const assert = require('assert');
 const WebpackHMRPlugin = require('../../src/webpack/WebpackHMRPlugin');
 
 describe('WebpackHMRPlugin', () => {
-    it('should have onBuildExit', () => {
+    it('should have onBuildExit', done => {
         let plugin = new WebpackHMRPlugin('localhost', 1337);
 
-        assert.equal(plugin.options.onBuildExit.length, 1);
-        assert.equal(plugin.options.onBuildExit[0], 'node ./triggerHMR.js localhost 1337');
+        const WebSocket = require('ws');
+        const wss = new WebSocket.Server({ port: 1337 });
+
+        wss.on('connection', (ws) => {
+            ws.on('message', (data) => {
+                assert(JSON.parse(data).HMR, true);
+                done();
+            });
+        });
+        
+        plugin.callback();
     });
 });
